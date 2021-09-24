5353aed9ec415384cb5b37dfacff3bf12b86fa35
phomie <marc.passenheim@gmx.net>
2021-09-24 10:59:05 +0200

    switch func google_ads_iframe_

--- src/utils/pageScraper.js
+++ src/utils/pageScraper.js
@@ -166,11 +166,7 @@ const scraperObject = {
         });
         console.log('tryToCheckthefunc', contentTyp);
 
-        //*** ADUNITSTRUCTUR_PROOF **********************************************
-        await page.waitForSelector("div[id^='google_ads_iframe_'] iframe", { visible: true }).then(() => {
-            console.log("iframe found")
 
-        })
 
         //*** adcallnizer****************************************************
 
@@ -276,8 +272,12 @@ const scraperObject = {
 
         });
         console.log('googletag', adcallnizer)
+            //*** ADUNITSTRUCTUR_PROOF **********************************************
+        await page.waitForSelector("div[id^='google_ads_iframe_'] iframe", { visible: true }).then(() => {
+                console.log("iframe found")
 
-        //VASTCHECK****************************************
+            })
+            //VASTCHECK****************************************
         try {
             thevidopla = await page.$("div.item-media__wrapper").then(console.log("vid found"))
             await page.evaluate(() => {
